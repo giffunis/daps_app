767ee6c6e3b111a6db694b61910f701625fa6192
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-03-25 15:50:19 +0000

    added a provisional toolbar

--- mobile/src/main/java/com/giffunis/dapsapp/BaseActivity.java
+++ mobile/src/main/java/com/giffunis/dapsapp/BaseActivity.java
@@ -2,43 +2,26 @@ package com.giffunis.dapsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class BaseActivity extends AppCompatActivity {
 
-    Drawer drawer;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
-
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(this.toolbar);
     }
 
-    public void addDrawable() {
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName("Item1");
-        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName("Item2");
 
-        drawer = new DrawerBuilder()
-                .withActivity(this)
-                .addDrawerItems(
-                        item1,
-                        new DividerDrawerItem(),
-                        item2)
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        Toast.makeText(BaseActivity.this, position, Toast.LENGTH_SHORT).show();
-                        return false;
-                    }
-                }).build();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
