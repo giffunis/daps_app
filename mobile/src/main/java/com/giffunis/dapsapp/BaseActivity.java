122d11783dbd94857ff3ef6fe332007e38ce71ec
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-03-25 15:27:55 +0000

    theme NoActionBar

--- mobile/src/main/java/com/giffunis/dapsapp/BaseActivity.java
+++ mobile/src/main/java/com/giffunis/dapsapp/BaseActivity.java
@@ -2,12 +2,43 @@ package com.giffunis.dapsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class BaseActivity extends AppCompatActivity {
+
+    Drawer drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+    }
+
+    public void addDrawable() {
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName("Item1");
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName("Item2");
+
+        drawer = new DrawerBuilder()
+                .withActivity(this)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        Toast.makeText(BaseActivity.this, position, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                }).build();
     }
 }
