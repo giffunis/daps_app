b9699218ccab2e984345c9d7151f9d229a0b7f61
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-03-26 22:29:48 +0000

    trying to comunicate Fragment and Activity, changes on Quiz List Fragment

--- mobile/src/main/java/MemoryTest/QuizesListFragment.java
+++ mobile/src/main/java/MemoryTest/QuizesListFragment.java
@@ -7,26 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.giffunis.dapsapp.R;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link QuizesListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
+
 public class QuizesListFragment extends Fragment {
 
     private ListView listView;
     private QuizArrayAdapter adapter;
-
-/*
-    private OnFragmentInteractionListener mListener;
-*/
+    private ArrayList<String> list;
+    OnQuizesListSelectedListener mCallback;
 
     public QuizesListFragment() {
         // Required empty public constructor
@@ -38,55 +32,38 @@ public class QuizesListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.listview_layout, container, false);
 
-        ArrayList<String> lista = getArguments().getStringArrayList("lista");
+        list = getArguments().getStringArrayList("lista");
 
-        adapter = new QuizArrayAdapter(getContext(),lista);
+        adapter = new QuizArrayAdapter(getContext(), list);
         listView = (ListView) view.findViewById(R.id.list_view);
         listView.setAdapter(this.adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCallback.startQuizSelected(list.get(position));
+            }
+        });
 
 
         return view;
+
     }
 
 
-
-
-    /*// TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public interface OnQuizesListSelectedListener{
+        public void startQuizSelected(String id);
     }
+
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+
+        try{
+            mCallback = (OnQuizesListSelectedListener) context;
+        }catch (ClassCastException e){
+            throw new ClassCastException(context.toString()
+                    + " must implement OnQuizesListSelectedListener");
         }
     }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    *//**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     *//*
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
 }
