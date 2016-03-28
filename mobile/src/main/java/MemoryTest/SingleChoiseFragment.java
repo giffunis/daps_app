e33fd1e29c608ac638423c9daef615c353c60ea8
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-03-28 16:18:12 +0100

    working on the SingleChoiseFragment.class

--- mobile/src/main/java/MemoryTest/SingleChoiseFragment.java
+++ mobile/src/main/java/MemoryTest/SingleChoiseFragment.java
@@ -6,14 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.giffunis.dapsapp.R;
+import com.orm.SugarRecord;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SingleChoiseFragment extends Fragment {
 
+    SingleChoiseArrayAdapter adapter;
+    ListView listView;
 
     public SingleChoiseFragment() {
         // Required empty public constructor
@@ -24,7 +29,13 @@ public class SingleChoiseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_single_choise, container, false);
+        View view = inflater.inflate(R.layout.listview_layout, container, false);
+
+        adapter = new SingleChoiseArrayAdapter(getContext(), quizesList);
+        listView = (ListView) view.findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
+        return view;
     }
 
 }
