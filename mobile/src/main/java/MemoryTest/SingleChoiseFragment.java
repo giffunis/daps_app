8b6b08489d0a5c6a7e4f01fb19daf7866e4daa95
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-04-05 18:34:51 +0100

    modified the SingleChoiseFragment

--- mobile/src/main/java/MemoryTest/SingleChoiseFragment.java
+++ mobile/src/main/java/MemoryTest/SingleChoiseFragment.java
@@ -10,8 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
-import com.giffunis.dapsapp.QuizesActivity;
 import com.giffunis.dapsapp.R;
 
 import java.util.ArrayList;
@@ -21,34 +19,30 @@ import java.util.ArrayList;
  */
 public class SingleChoiseFragment extends Fragment {
 
-    private static final String CURRENT_QUESTION_ID = "qId";
     private static final String BODY_QUESTION = "question";
     private static final String ANSWERS_LIST = "answers";
     private static final String USER_ANSWER = "userAnswer";
 
-    ArrayList<String> answersList;
-    String bodyQuestion;
-    int quizId;
-
+    ArrayList<String> answersList_;
+    String bodyQuestion_;
     OnSingleChoiseSelectListener mCallback;
 
     public SingleChoiseFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.listview_layout, container, false);
-        SingleChoiseArrayAdapter adapter = new SingleChoiseArrayAdapter(getContext(), answersList);
+        SingleChoiseArrayAdapter adapter = new SingleChoiseArrayAdapter(getContext(), answersList_);
         ListView listView = (ListView) view.findViewById(R.id.list_view);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                mCallback.singleChoiseResult(quizId,answersList.get(position),position + 1);
+                mCallback.singleChoiseResult(bodyQuestion_,answersList_.get(position));
             }
         });
 
@@ -58,11 +52,9 @@ public class SingleChoiseFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
         final Resources resources = context.getResources();
-        answersList = getArguments().getStringArrayList(ANSWERS_LIST);
-        bodyQuestion = getArguments().getString(BODY_QUESTION);
-        quizId = getArguments().getInt(CURRENT_QUESTION_ID);
+        answersList_ = getArguments().getStringArrayList(ANSWERS_LIST);
+        bodyQuestion_ = getArguments().getString(BODY_QUESTION);
 
         try{
             mCallback = (OnSingleChoiseSelectListener) context;
@@ -74,6 +66,6 @@ public class SingleChoiseFragment extends Fragment {
     }
 
     public interface OnSingleChoiseSelectListener{
-        public void singleChoiseResult(int qId, String userAnswer, int aId);
+        public void singleChoiseResult(String bodyQuestion, String userAnswer);
     }
 }
