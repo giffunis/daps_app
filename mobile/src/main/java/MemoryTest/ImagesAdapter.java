d270cfeb64e27262ea71178f22cfcb05aac22d2c
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-04-06 15:49:44 +0100

    created the imageAdapter for the RecyclerView

---
+++ mobile/src/main/java/MemoryTest/ImagesAdapter.java
@@ -0,0 +1,26 @@
+package MemoryTest;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by drcaspa on 6/4/16.
+ * email: giffunis@gmail.com
+ */
+public class ImagesAdapter extends RecyclerView.Adapter<String> {
+
+    @Override
+    public String onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(String holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
