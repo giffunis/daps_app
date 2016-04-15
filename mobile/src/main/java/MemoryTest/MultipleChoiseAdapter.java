d0cadc9eebbd086b1156d582e86a8fcf5ddda938
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-04-15 21:34:05 +0100

    modified the adapter

--- mobile/src/main/java/MemoryTest/MultipleChoiseAdapter.java
+++ mobile/src/main/java/MemoryTest/MultipleChoiseAdapter.java
@@ -16,9 +16,9 @@ import java.util.ArrayList;
  * Created by drcaspa on 12/4/16.
  * email: giffunis@gmail.com
  */
-public class MultipleChoiseAdapter extends ArrayAdapter<String> {
+public class MultipleChoiseAdapter extends ArrayAdapter<Row> {
 
-    public MultipleChoiseAdapter(Context context, ArrayList<String> answers) {
+    public MultipleChoiseAdapter(Context context, ArrayList<Row> answers) {
         super(context, 0, answers);
     }
 
@@ -42,10 +42,21 @@ public class MultipleChoiseAdapter extends ArrayAdapter<String> {
         ImageView categoria = (ImageView)listItemView.findViewById(R.id.category);
 
         //Obteniendo instancia de la Tarea en la posición actual
-        String answer = getItem(position);
+        final Row answer = getItem(position);
 
-        checkBox.setText(answer);
+        checkBox.setText(answer.getPhrase_());
         categoria.setImageResource(R.drawable.ic_tests);
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (answer.isChecked_() == true) {
+                    answer.setChecked_(false);
+                }else{
+                    answer.setChecked_(true);
+                }
+            }
+        });
 
 
         return listItemView;
