998b6833aa4ef641653cef9e3b73f9593e290253
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-04-15 21:19:50 +0100

    Class finished

--- mobile/src/main/java/MemoryTest/Row.java
+++ mobile/src/main/java/MemoryTest/Row.java
@@ -5,4 +5,29 @@ package MemoryTest;
  * email: giffunis@gmail.com
  */
 public class Row {
+    private String phrase_;
+    private boolean checked_;
+
+    public Row(){}
+
+    public Row(String phrase){
+        phrase_ = phrase;
+        checked_ = false;
+    }
+
+    public String getPhrase_() {
+        return phrase_;
+    }
+
+    public void setPhrase_(String phrase_) {
+        this.phrase_ = phrase_;
+    }
+
+    public boolean isChecked_() {
+        return checked_;
+    }
+
+    public void setChecked_(boolean checked_) {
+        this.checked_ = checked_;
+    }
 }
