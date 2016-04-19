a6f193456e7d841afd0f8a2c540df92abfc6d100
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-04-19 19:06:02 +0100

    added a button to the activity base

--- mobile/src/main/java/com/giffunis/dapsapp/BaseActivity.java
+++ mobile/src/main/java/com/giffunis/dapsapp/BaseActivity.java
@@ -28,6 +28,32 @@ public class BaseActivity extends AppCompatActivity {
         initNavDrawer();
     }
 
+    public void jsonDownload(){
+        TextView mTxtDisplay;
+        ImageView mImageView;
+        mTxtDisplay = (TextView) findViewById(R.id.txtDisplay);
+        String url = "http://my-json-feed";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mTxtDisplay.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+    }
+
     private void initNavDrawer(){
         PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName(R.string.home).withIdentifier(1);
         PrimaryDrawerItem item2 = new PrimaryDrawerItem().withName(R.string.quizes).withIdentifier(2);
