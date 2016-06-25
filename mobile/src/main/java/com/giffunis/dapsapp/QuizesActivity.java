7463fb71f52ad4b7b43a7f335917730b096ef7aa
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-25 17:42:39 +0100

    Ahora envia un String

--- mobile/src/main/java/com/giffunis/dapsapp/QuizesActivity.java
+++ mobile/src/main/java/com/giffunis/dapsapp/QuizesActivity.java
@@ -312,7 +312,7 @@ public class QuizesActivity extends AppCompatActivity implements
         for (int i = 0; i < currentUserAnswers_.getN_(); i++){
             questionAnswer = new JSONObject();
             //questionAnswer.put("bodyQuestion", currentUserAnswers_.getBodyQuestion(i));
-            //questionAnswer.put("bodyQuestion", "Holá");
+            questionAnswer.put("bodyQuestion", "Holá");
             questionAnswer.put("answer", currentUserAnswers_.getUserAnswer(i));
             questionAnswer.put("answerType", currentUserAnswers_.getAnswerType(i));
             questionAnswer.put("isCorrect", currentUserAnswers_.getIsCorrect(i));
@@ -329,10 +329,10 @@ public class QuizesActivity extends AppCompatActivity implements
         Toast firmaProblems = Toast.makeText(getApplicationContext(),"Mensaje No Firmado",Toast.LENGTH_SHORT);
         try {
             mensaje = mensaje.replace("\\","");
-            //mensaje = mensaje.replace('á','a');
+            mensaje = mensaje.replace('á','a');
             signature = Firmar.firmar(mensaje);
             respuestaCompleta.put(SIGNATURE,signature);
-            respuestaCompleta.put(MENSAJE,solvedQuiz);
+            respuestaCompleta.put(MENSAJE,mensaje);
             firmaOk.show();
 
         } catch (UnsupportedEncodingException e) {
