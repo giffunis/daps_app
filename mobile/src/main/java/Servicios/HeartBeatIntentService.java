2eab9a2d29b56033175a17d69ea9e309c2882796
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-07-09 00:01:57 +0100

    Eliminando cosas que no se utilizan

--- mobile/src/main/java/Servicios/HeartBeatIntentService.java
+++ mobile/src/main/java/Servicios/HeartBeatIntentService.java
@@ -2,10 +2,7 @@ package servicios;
 
 import android.app.IntentService;
 import android.app.NotificationManager;
-import android.app.Service;
 import android.content.Intent;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.Handler;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
